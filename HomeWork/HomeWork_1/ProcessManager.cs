27680be8b1f101b67503c69e6152cc86713000e0
Alexander <dreadnight@tut.by>
2018-02-05 17:14:39 +0300

    Project ready

---
+++ HomeWork/HomeWork_1/ProcessManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_1
+{
+    public class ProcessManager
+    {
+        private Process[] processes;
+
+        public ProcessManager()
+        {
+            processes = processes = Process.GetProcesses(".");
+        }
+        public Process[] GetAllProcesses()
+        {
+            return processes;
+        }
+        public Process GetProcess(int id)
+        {
+            return processes.FirstOrDefault(pro => pro.Id == id);
+        }
+        public Process StartProcess(string fileName)
+        {
+            return Process.Start(fileName);
+        }
+        public void CloseProcess(string nameProcess)
+        {
+            Process.GetProcessesByName(nameProcess)[0].Close();
+        }
+        public ProcessModuleCollection GetModules(string nameProcess)
+        {
+            return Process.GetProcessesByName(nameProcess)[0].Modules;
+        }
+        public ProcessThreadCollection GetThreads(string nameProcess)
+        {
+            return Process.GetProcessesByName(nameProcess)[0].Threads;
+        }
+    }
+}
