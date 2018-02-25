c5ebec06ae0b85caf6712de7232c3945cd9435a7
Alexander <dreadnight@tut.by>
2018-02-25 13:23:18 +0300

    Add project

--- HomeWork/HomeWork_3/Program.cs
+++ HomeWork/HomeWork_3/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HomeWork_3
@@ -10,6 +11,51 @@ namespace HomeWork_3
     {
         static void Main(string[] args)
         {
+            var S = new List<double>();
+            var R = new List<double>();
+
+
+            var A = new Thread(() =>
+            {
+                var i = 0;
+                while (true)
+                { S.Add(i++); }
+            });
+
+            A.Start();
+
+            var B = new Thread(() =>
+            {
+                while (true)
+                    if (S.Count == 0)
+                        Thread.Sleep(1000);
+                    else
+                        { R.Add(Math.Pow(S.Last(), 2)); }
+                });
+
+            B.Start();
+
+            var C = new Thread(() =>
+            {
+                while (true)
+                    if (S.Count == 0)
+                        Thread.Sleep(1000);
+                    else
+                        { R.Add(S.Last() / 3); }
+                });
+
+            C.Start();
+
+            var D = new Thread(() =>
+            {
+                while (true)
+                    if (R.Count == 0)
+                        { Console.WriteLine("List R empty!"); Thread.Sleep(1000); }
+                    else
+                        { Console.WriteLine(R.Last()); }
+                });
+            D.Start();
+
         }
     }
 }
